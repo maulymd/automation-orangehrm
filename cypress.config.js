3af36c8d0b87f73cb7834189fa692031ed804091
maulymd <maulanayusufahmd@gmail.com>
2026-02-14 23:40:38 +0700

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    experimentalStudio: true,
+    baseUrl: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,6 @@
+describe("Authentication", () => {
+  it("login", () => {
+    cy.visit("/");
+    
+  });
+});
